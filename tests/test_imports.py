6e7ce6f8972af9959da2e48a6aa188fa05cc5d10
Sudhanshu Misra <103576112+sudhanshu-misra@users.noreply.github.com>
2025-11-30 02:53:14 +0530

    Initial commit

---
+++ tests/test_imports.py
@@ -0,0 +1,7 @@
+def test_imports():
+    import hotel_bookings_pipeline.data_loader
+    import hotel_bookings_pipeline.preprocess
+    import hotel_bookings_pipeline.feature_engineering
+    import hotel_bookings_pipeline.train
+    import hotel_bookings_pipeline.evaluate
+    import hotel_bookings_pipeline.main
